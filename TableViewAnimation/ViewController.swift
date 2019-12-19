2b189fa79484d8eeaacb7fc05cd0da620b5eca25
Renan Dias <renan@jera.com.br>
2019-12-19 19:57:14 -0400

    Initial Commit

---
+++ TableViewAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewAnimation
+//
+//  Created by Renan Benatti Dias on 19/12/19.
+//  Copyright © 2019 Blue Stack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TableViewAnimationTests/TableViewAnimationTests.swift
@@ -0,0 +1,34 @@
+//
+//  TableViewAnimationTests.swift
+//  TableViewAnimationTests
+//
+//  Created by Renan Benatti Dias on 19/12/19.
+//  Copyright © 2019 Blue Stack. All rights reserved.
+//
+
+import XCTest
+@testable import TableViewAnimation
+
+class TableViewAnimationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
